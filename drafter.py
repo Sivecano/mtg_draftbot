3b2225c3cd2ca2e3a78a2c92a3d4a5916c6121c5
Sivecano <Sivecano@gmail.com>
2021-12-04 21:52:38 +0100

    significantly improved the drafter. it will now only try to display images in kitty

--- drafter.py
+++ drafter.py
@@ -3,7 +3,7 @@ import pandas, random, os
 data = pandas.read_csv("set.csv")
 
 
-print({r : len(data[data.rarity==r]) for r in set(data.rarity)})
+#print({r : len(data[data.rarity==r]) for r in set(data.rarity)})
 
 basic_lands = data.query("type.str.contains('Basic Land')")
 commons = data.query("rarity=='common' and not type.str.contains('Basic Land')")
@@ -13,8 +13,20 @@ mythics = data[data.rarity=="mythic"]
 mythrares = mythics.append(rares)
 #print(mythrares)
 
-deck = commons.sample(10, replace=True).append(uncommons.sample(3, replace=True)).append(mythrares.sample())
-print(deck)
+def draft():
+    return list(commons.sample(10, replace=True).append(
+                uncommons.sample(3, replace=True)).append(
+                    mythrares.sample())
+                .uuid)
 
-os.system(f"timg -p k --grid=4 {' '.join(map(lambda x : os.path.join('images', x), list(deck.uuid)))}")
+def nameprint(deck):
+    for i in set(deck):
+        print(deck.count(i), "x", data.query("uuid == @i")["name"].values[0])
+
+if __name__ == "__main__":
+    deck = draft()
+    #print(data.query("uuid in @deck"))
+    nameprint(deck)
+    if "kitty" in os.environ["TERM"].lower():
+        os.system(f"timg -p k --grid=4 {' '.join(map(lambda x : os.path.join('images', x), deck))}")
 
