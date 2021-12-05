baecf40cf26d1b4c563d61e04bebde8dbb85f587
Sivecano <Sivecano@gmail.com>
2021-12-05 09:39:09 +0100

    we use png now instead of medium jpeg

--- imagegetter.py
+++ imagegetter.py
@@ -8,14 +8,13 @@ current = 0
 print(len(data))
 print("      ", end="")
 
-for (id, image) in (zip(data["uuid"], data["image"])):
+for (id, image) in zip(data["uuid"], data["image"]):
     with requests.get(image) as img:
         with open(os.path.join("images", id), "wb") as file:
             file.write(img.content)
     current += 1
-    print(6*chr(8), end="")
-    print(round(100*current / cards, 2), "%", end="", flush=True)
-    time.sleep(0.07)
+    print(7*chr(8), end="")
+    print(str(round(100*current / cards, 2)).zfill(5), "%", end="", flush=True)
     
-
+print()
 
--- printer.py
+++ printer.py
@@ -2,7 +2,7 @@ import pandas, os
 
 data = pandas.read_csv("set.csv")
 
-mythics = data[data.rarity=='rare']
+mythics = data[data.rarity=='mythic']
 
 os.system(f"timg -p k --grid=3 {' '.join(map(lambda x : os.path.join('images', x), list(mythics.uuid)))}")
 
